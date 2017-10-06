402a18645a37c71985e09569adc97ad4da6b8575
Malcolm Jones <malcolm@adobe.com>
2017-10-05 23:03:26 -0400

    wip: small fab update

--- fabfile.py
+++ fabfile.py
@@ -51,7 +51,7 @@ env.shell= '/bin/sh -l -c'
 
 # export DOCKER_TLS_VERIFY="1"
 # export DOCKER_HOST="tcp://192.168.99.101:2376"
-# export DOCKER_CERT_PATH="/Users/malcolm/.docker/machine/machines/local-mesos-cluster"
+# export DOCKER_CERT_PATH="/Users/username/.docker/machine/machines/local-mesos-cluster"
 # export DOCKER_MACHINE_NAME="local-mesos-cluster"
 # # Run this command to configure your shell:
 # # eval $(docker-machine env local-mesos-cluster)
