3d2607dd584696aeab14fb715597464223b750de
Malcolm Jones <malcolm@adobe.com>
2017-11-14 23:26:03 -0500

    chg: set env linux

--- scripts/setup_env_linux.sh
+++ scripts/setup_env_linux.sh
@@ -8,3 +8,11 @@ export DOCKER_IP=$HOST_IP
 export DOCKER_HOST="tcp://${DOCKER_IP}:2377"
 export PATH_TO_DOCKER=$(which docker)
 export YOUR_HOSTNAME=$(hostname | cut -d "." -f1 | awk '{print $1}')
+
+echo "******************************************"
+echo "HOST_IP: ${HOST_IP}"
+echo "DOCKER_IP: ${DOCKER_IP}"
+echo "DOCKER_HOST: ${DOCKER_HOST}"
+echo "PATH_TO_DOCKER: ${PATH_TO_DOCKER}"
+echo "YOUR_HOSTNAME: ${YOUR_HOSTNAME}"
+echo "******************************************"
